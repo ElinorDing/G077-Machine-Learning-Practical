72d0aceeb6a723eb05b2a24b1bafc43d69e21dc2
ElinorDing <47607489+ElinorDing@users.noreply.github.com>
2023-03-11 20:48:24 +0000

    update file

--- Fine_tuning_ViT.py
+++ Fine_tuning_ViT.py
@@ -5,8 +5,25 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import cv2
 from datasets import load_dataset
+from transformers import ViTImageProcessor
+from transformers import ViTForImageClassification
+from transformers import TrainingArguments, Trainer
+from torch.utils.data import DataLoader
+import torch
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from torchvision.transforms import (CenterCrop,
+                                    Compose, 
+                                    Normalize, 
+                                    RandomHorizontalFlip,
+                                    RandomResizedCrop, 
+                                    Resize, 
+                                    ToTensor)
 
-dataset = load_dataset("imagefolder", data_dir="../Data_to_use")
+
+
+
+dataset = load_dataset("imagefolder", data_dir="../G077-Machine-Learning-Practical/Data")
 
 splits = dataset["train"].train_test_split(test_size=0.1)
 
@@ -16,3 +33,110 @@ train_ds = splits_2["train"]
 val_ds = splits_2["test"]
 
 print(train_ds[0]['label'])
+
+id2label = {id:label for id, label in enumerate(train_ds.features['label'].names)}
+label2id = {label:id for id,label in id2label.items()}
+
+print(id2label)
+
+processor = ViTImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k")
+
+image_mean, image_std = processor.image_mean, processor.image_std
+size = processor.size["height"]
+
+normalize = Normalize(mean=image_mean, std=image_std)
+_train_transforms = Compose(
+        [
+            RandomResizedCrop(size),
+            RandomHorizontalFlip(),
+            ToTensor(),
+            normalize,
+        ]
+    )
+
+_val_transforms = Compose(
+        [
+            Resize(size),
+            CenterCrop(size),
+            ToTensor(),
+            normalize,
+        ]
+    )
+
+def train_transforms(examples):
+    examples['pixel_values'] = [_train_transforms(image.convert("RGB")) for image in examples['image']]
+    return examples
+
+def val_transforms(examples):
+    examples['pixel_values'] = [_val_transforms(image.convert("RGB")) for image in examples['image']]
+    return examples
+
+# Set the transforms
+train_ds.set_transform(train_transforms)
+val_ds.set_transform(val_transforms)
+test_ds.set_transform(val_transforms)
+
+def collate_fn(examples):
+    pixel_values = torch.stack([example["pixel_values"] for example in examples])
+    labels = torch.tensor([example["label"] for example in examples])
+    return {"pixel_values": pixel_values, "labels": labels}
+
+train_dataloader = DataLoader(train_ds, collate_fn=collate_fn, batch_size=4)
+
+batch = next(iter(train_dataloader))
+for k,v in batch.items():
+  if isinstance(v, torch.Tensor):
+    print(k, v.shape)
+
+model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224-in21k',
+                                                  id2label=id2label,
+                                                  label2id=label2id)
+
+metric_name = "accuracy"
+
+args = TrainingArguments(
+    f"test-brain_tumor_classification",
+    save_strategy="epoch",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=10,
+    per_device_eval_batch_size=4,
+    # training epoch, could be changed later 
+    num_train_epochs=10,
+    weight_decay=0.01,
+    load_best_model_at_end=True,
+    metric_for_best_model=metric_name,
+    logging_dir='logs',
+    remove_unused_columns=False,
+)
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return dict(accuracy=accuracy_score(predictions, labels))
+
+trainer = Trainer(
+    model,
+    args,
+    train_dataset=train_ds,
+    eval_dataset=val_ds,
+    data_collator=collate_fn,
+    compute_metrics=compute_metrics,
+    tokenizer=processor,
+)
+
+trainer.train()
+outputs = trainer.predict(test_ds)
+print(outputs.metrics)
+
+y_true = outputs.label_ids
+y_pred = outputs.predictions.argmax(1)
+
+labels = train_ds.features['label'].names
+cm = confusion_matrix(y_true, y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+disp.plot(xticks_rotation=45)
+
+trainer.save_model(output_dir="../Output")
+
+
