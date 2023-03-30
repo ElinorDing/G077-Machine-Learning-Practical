e7bf9b8609951259bad83546d3c0444a0e6dee99
dyt <vonnet@dyts-MacBook-Air.local>
2023-03-31 00:20:26 +0100

    update

--- plotGraph.py
+++ plotGraph.py
@@ -1,47 +1,37 @@
-import matplotlib.pyplot as plt
-import numpy as np 
+# 
 
+import tensorflow as tf
+from tensorboard.plugins import projector
+import numpy as np
 
-def multi_plot(resnet, vgg, vit,interval):
-    # plt.plot(interval, resnet,'+-', label = 'RESNET')
-    # plt.plot(interval, vgg,'+-', label = 'VGG16')
-    # plt.plot(interval, vit,'+-', label = 'VIT')
-    X_axis = np.arange(len(interval))
-    plt.bar(X_axis-0.2, resnet,0.2, label = 'RESNET')
-    plt.bar(X_axis, vgg, 0.2, label = 'VGG16')
-    plt.bar(X_axis+0.2, vit, 0.2,label = 'VIT')
-
-    # plt.suptitle('Evaluation with different intervals', fontsize = 15)
-    # my_xticks = ['<=13','13 ~ 19','19 ~ 25','25 ~ 31','>31']
-    plt.xticks(X_axis,interval)
-    # plt.ylim(88,100)
-    plt.margins(x=0)
-    # plt.legend(['ROUGLE-L','Exact Match','BLEU'])
-    plt.legend()
-    # plt.xlabel('Various Length interval')
-    plt.xlabel('Data Size for Training (%)')
-    plt.ylabel('F1 Score(%)')
-    # plt.show()
-    plt.savefig('vary_training_accuracy')
-
-
-
-# varing training size to evaluate accuracy
+# Define the data for the three models
 resnet = [90.248, 93.0348,92.039,93.432]
 vgg16 = [90.54,97.80,97.94,98.59]
 vit = [97.51,97.76,98.08,98.62]
 training_size = [25, 50, 75, 100]
 
-# varing training size to evaluate loss
-# resnet = [0.2875,0.2356,0.2084,0.1806]
-# vgg16 = [0.3635,0.2584,0.1426,0.1066]
-# vit = [0.2817,0.1970,0.1638,0.1511]
-# training_size = [25, 50, 75, 100]
+# Create a summary writer for TensorBoard
+writer = tf.summary.create_file_writer('logs')
 
-# # varing training size to evaluate f1
-# resnet = [90.233,93.078,92.039,93.430]
-# vgg16 = [26.59,22.88,26.75,24.38]
-# vit = [97.52,97.75,98.08,98.63]
-# training_size = [25, 50, 75, 100]
+# Create a step variable for the x-axis
+step = 0
 
-multi_plot(resnet,vgg16,vit,training_size)
+# Loop through the data and write each value to TensorBoard
+for r, v, vi, t in zip(resnet, vgg16, vit, training_size):
+    with writer.as_default():
+        # Write the accuracy for each model
+        tf.summary.scalar('ResNet', r, step=step)
+        tf.summary.scalar('VGG16', v, step=step)
+        tf.summary.scalar('ViT', vi, step=step)
+        # Write the training data size for each iteration
+        tf.summary.scalar('Training Size', t, step=step)
+        
+    step += 1
+
+# Close the summary writer
+writer.close()
+
+# Write the accuracy for each model with smoothing
+tf.summary.scalar('ResNet', r, step=step, smoothing_exponent=0.6)
+tf.summary.scalar('VGG16', v, step=step, smoothing_exponent=0.6)
+tf.summary.scalar('ViT', vi, step=step, smoothing_exponent=0.6)
