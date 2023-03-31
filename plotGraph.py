b7f9ecf2ab3fcb7214e4530e3c6241ad9cd42264
dyt <vonnet@dyts-MacBook-Air.local>
2023-03-31 01:13:43 +0100

    update

--- plotGraph.py
+++ plotGraph.py
@@ -20,17 +20,17 @@ def multi_plot(resnet, vgg, vit,interval):
     plt.legend()
     # plt.xlabel('Various Length interval')
     plt.xlabel('Data Size for Training (%)')
-    plt.ylabel('Accuracy(%)')
+    plt.ylabel('F1 Score(%)')
     # plt.show()
-    plt.savefig('vary_training_accuracy')
+    plt.savefig('vary_training_f1')
 
 
 
 # varing training size to evaluate accuracy
-resnet = [90.248, 93.0348,92.039,93.432]
-vgg16 = [90.54,97.80,97.94,98.59]
-vit = [97.51,97.76,98.08,98.62]
-training_size = [25, 50, 75, 100]
+# resnet = [90.248, 93.0348,92.039,93.432]
+# vgg16 = [90.54,97.80,97.94,98.59]
+# vit = [97.51,97.76,98.08,98.62]
+# training_size = [25, 50, 75, 100]
 
 # varing training size to evaluate loss
 # resnet = [0.2875,0.2356,0.2084,0.1806]
@@ -39,9 +39,9 @@ training_size = [25, 50, 75, 100]
 # training_size = [25, 50, 75, 100]
 
 # # varing training size to evaluate f1
-# resnet = [90.233,93.078,92.039,93.430]
-# vgg16 = [26.59,22.88,26.75,24.38]
-# vit = [97.52,97.75,98.08,98.63]
-# training_size = [25, 50, 75, 100]
+resnet = [90.233,93.078,92.039,93.430]
+vgg16 = [26.59,22.88,26.75,24.38]
+vit = [97.52,97.75,98.08,98.63]
+training_size = [25, 50, 75, 100]
 
 multi_plot(resnet,vgg16,vit,training_size)
